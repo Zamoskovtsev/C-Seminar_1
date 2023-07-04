4288720954826e02cff7b8ba7f12372bfa829759
Zamoskovtsev <138019962+Zamoskovtsev@users.noreply.github.com>
2023-07-04 23:11:59 +0300

    Update HomeTask#8.cs

--- HomeTask#8.cs
+++ HomeTask#8.cs
@@ -1,1 +1,12 @@
-HOmeTas
+//Задача 8
+Console.Write("Введите целое число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int ResDel;
+for (int i=1; i<=Number; i++)
+{
+    ResDel=i%2;
+ if (ResDel==0)
+   {
+     Console.WriteLine($"Число  {i}  четное");
+   }
+}
