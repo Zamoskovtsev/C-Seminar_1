882661f4a4f9f22a7ae11654fefb431ba09a63c3
Zamoskovtsev <138019962+Zamoskovtsev@users.noreply.github.com>
2023-07-04 22:51:50 +0300

    Update HomeTask#4.cs

--- HomeTask#4.cs
+++ HomeTask#4.cs
@@ -1,1 +1,16 @@
-
+Console.Write("Введите первое целое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+int MaxRes=Number1;
+if (Number1<Number2)
+   {
+     MaxRes=Number2;
+   }
+  if (MaxRes<Number3)
+   { 
+     MaxRes=Number3;
+   } 
+     Console.Write($"Max {MaxRes}");   
