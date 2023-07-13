b66c21df72345c30953af08ac1ec75b8526be158
Zamoskovtsev <138019962+Zamoskovtsev@users.noreply.github.com>
2023-07-13 11:34:15 +0300

    Create NomeTask#19.cs

---
+++ NomeTask#19.cs
@@ -0,0 +1,17 @@
+// Задача 19
+Console.Write($"Введите любое целое число:  ");
+int Number1=Convert.ToInt32(Console.ReadLine());
+String StrNum = Convert.ToString(Number1);
+int Len = StrNum.Length;
+int i = 1;
+int n = (Len-1)/2;
+Boolean Flag = true;
+if (Len%2==0){n = Len/2;}
+while (i<=n)
+     {
+         if (StrNum.Substring(i-1,1)==StrNum.Substring(Len-i,1)){Flag=true;}
+         else{Flag=false;}   
+         i++;
+     }
+     if (Flag==true){Console.Write($"Число {Number1} {Flag} является палиндромом");}
+     else{Console.Write($"Число {Number1} {Flag} не является палиндромом");}
