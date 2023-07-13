89b298ea4d013e71f8c8d02b8f38a39e1bc17508
Zamoskovtsev <138019962+Zamoskovtsev@users.noreply.github.com>
2023-07-13 11:36:39 +0300

    Create HomeTask#23.cs

---
+++ HomeTask#23.cs
@@ -0,0 +1,8 @@
+// Задача 23
+Console.Write($"Введите целое число:  ");
+int Number=Convert.ToInt32(Console.ReadLine());
+int i=0;
+for (i=0; i<Number+1; i++)
+ {
+     Console.WriteLine($"Куб числа {i} равен {Math.Pow(i,3)}");
+ }
